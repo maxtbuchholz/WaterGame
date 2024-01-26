2dcb6ad89799a986391323d323450ad4237ab0c1
maxtbuchholz <92826974+maxtbuchholz@users.noreply.github.com>
2024-01-25 21:06:03 -0600

    layered foam

---
+++ Assets/RenderTexture/MaskLowerLayers/MaskObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskObject : MonoBehaviour
+{
+    [SerializeField] GameObject[] maskedObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < maskedObjects.Length; i++)
+        {
+            maskedObjects[i].GetComponent<MeshRenderer>().material.renderQueue = 3002;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
