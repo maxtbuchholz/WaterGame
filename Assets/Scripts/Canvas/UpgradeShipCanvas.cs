2897a8218748b32fcf97c042754acffec51da963
maxtbuchholz <92826974+maxtbuchholz@users.noreply.github.com>
2024-03-10 23:16:00 -0500

    exchange ship screen

--- Assets/Scripts/Canvas/UpgradeShipCanvas.cs
+++ Assets/Scripts/Canvas/UpgradeShipCanvas.cs
@@ -6,7 +6,11 @@ public class UpdradeShipCanvas : MonoBehaviour
 {
     [SerializeField] RectTransform shipFrame;
     [SerializeField] RectTransform buttonFrame;
+    [SerializeField] RectTransform exchangeFrame;
     [SerializeField] ShipUpgradeButtons shipUpgradeButtons;
+    [SerializeField] GameObject shipExchangeCanvas;
+    [SerializeField] List<GameObject> upgradeObjects;
+    [SerializeField] List<GameObject> excahangeObjects;
     int screenHeight;
     int screenWidth;
     Vector2 currentPos;
@@ -21,9 +25,74 @@ public class UpdradeShipCanvas : MonoBehaviour
         shipFrame.sizeDelta = new Vector2(100, screenHeight * 0.8f);
         buttonFrame.anchoredPosition = currentPos;
         buttonFrame.sizeDelta = new Vector2(100, screenHeight * 0.8f);
+        exchangeFrame.anchoredPosition = currentPos;
+        exchangeFrame.sizeDelta = new Vector2(100, screenHeight * 0.8f);
+    }
+    public void ToExchangeClicked()
+    {
+        StartCoroutine(ToUpgradeSlide(false));
+    }
+    public void BackFromExchangeClicked()
+    {
+        StartCoroutine(ToUpgradeSlide(true));
+    }
+    private IEnumerator ToUpgradeSlide(bool toUp)
+    {
+        float timeToSlide = 0.2f;
+        float width = Screen.width;
+        float time = 0;
+        if (toUp)
+        {
+            while (time < timeToSlide)
+            {
+            time += Time.deltaTime;
+                foreach (GameObject go in upgradeObjects)
+                    go.GetComponent<RectTransform>().anchoredPosition = new Vector2(Mathf.Lerp(-width, 0, time / timeToSlide), go.GetComponent<RectTransform>().anchoredPosition.y);
+                foreach (GameObject go in excahangeObjects)
+                    go.GetComponent<RectTransform>().anchoredPosition = new Vector2(Mathf.Lerp(0, width, time / timeToSlide), go.GetComponent<RectTransform>().anchoredPosition.y);
+                yield return null;
+            }
+            ToUpgradeImediate(true);
+        }
+        else
+        {
+            while (time < timeToSlide)
+            {
+                time += Time.deltaTime;
+                foreach (GameObject go in upgradeObjects)
+                    go.GetComponent<RectTransform>().anchoredPosition = new Vector2(Mathf.Lerp(0, -width, time / timeToSlide), go.GetComponent<RectTransform>().anchoredPosition.y);
+                foreach (GameObject go in excahangeObjects)
+                    go.GetComponent<RectTransform>().anchoredPosition = new Vector2(Mathf.Lerp(width, 0, time / timeToSlide), go.GetComponent<RectTransform>().anchoredPosition.y);
+                yield return null;
+            }
+            ToUpgradeImediate(false);
+        }
+    }
+    private void ToUpgradeImediate(bool toUp)
+    {
+        //foreach (GameObject go in upgradeObjects)
+        //    go.SetActive(toUp);
+        //foreach (GameObject go in excahangeObjects)
+        //    go.SetActive(!toUp);
+        float width = Screen.width;
+        if (toUp)
+        {
+            foreach (GameObject go in upgradeObjects)
+                go.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, go.GetComponent<RectTransform>().anchoredPosition.y);
+            foreach (GameObject go in excahangeObjects)
+                go.GetComponent<RectTransform>().anchoredPosition = new Vector2(width, go.GetComponent<RectTransform>().anchoredPosition.y);
+        }
+        else
+        {
+            foreach (GameObject go in excahangeObjects)
+                go.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, go.GetComponent<RectTransform>().anchoredPosition.y);
+            foreach (GameObject go in upgradeObjects)
+                go.GetComponent<RectTransform>().anchoredPosition = new Vector2(-width, go.GetComponent<RectTransform>().anchoredPosition.y);
+        }
     }
     public void MoveToFrame()
     {
+        ToUpgradeImediate(true);
         StartCoroutine(MoveToCenter(0.2f));
         shipUpgradeButtons.SetShipKey();
     }
@@ -43,13 +112,15 @@ public class UpdradeShipCanvas : MonoBehaviour
             if (currentTime > timeToCenter) currentTime = timeToCenter;
             float setHeight = initialHeight + (heightDiff * (currentTime / timeToCenter));
             currentPos = new Vector2(0, setHeight);
-            shipFrame.anchoredPosition = currentPos;
-            buttonFrame.anchoredPosition = currentPos;
+            shipFrame.anchoredPosition = new Vector2(shipFrame.anchoredPosition.x, currentPos.y);
+            buttonFrame.anchoredPosition = new Vector2(buttonFrame.anchoredPosition.x, currentPos.y);
+            exchangeFrame.anchoredPosition = new Vector2(exchangeFrame.anchoredPosition.x, currentPos.y);
             yield return null;
         }
         currentPos = Vector2.zero;
-        shipFrame.anchoredPosition = currentPos;
-        buttonFrame.anchoredPosition = currentPos;
+        shipFrame.anchoredPosition = new Vector2(shipFrame.anchoredPosition.x, currentPos.y);
+        exchangeFrame.anchoredPosition = new Vector2(exchangeFrame.anchoredPosition.x, currentPos.y);
+        buttonFrame.anchoredPosition = new Vector2(buttonFrame.anchoredPosition.x, currentPos.y);
         StartCoroutine(MoveToExpand(0.2f));
     }
     IEnumerator MoveToExpand(float timeToCenter)
@@ -65,10 +136,12 @@ public class UpdradeShipCanvas : MonoBehaviour
             float setSize = initialSize + (widthDiff * (currentTime / timeToCenter));
             shipFrame.sizeDelta = new Vector2(setSize, shipFrame.rect.height);
             buttonFrame.sizeDelta = new Vector2(setSize, shipFrame.rect.height);
+            exchangeFrame.sizeDelta = new Vector2(setSize, shipFrame.rect.height);
             yield return null;
         }
         shipFrame.sizeDelta = new Vector2(finalWidth, shipFrame.rect.height);
         buttonFrame.sizeDelta = new Vector2(finalWidth, shipFrame.rect.height);
+        exchangeFrame.sizeDelta = new Vector2(finalWidth, shipFrame.rect.height);
     }
     IEnumerator MoveToBottom(float timeToCenter)
     {
@@ -84,11 +157,13 @@ public class UpdradeShipCanvas : MonoBehaviour
             currentPos = new Vector2(0, setHeight);
             shipFrame.anchoredPosition = currentPos;
             buttonFrame.anchoredPosition = currentPos;
+            exchangeFrame.anchoredPosition = currentPos;
             yield return null;
         }
         currentPos = new Vector2(0, finalHeight);
         shipFrame.anchoredPosition = currentPos;
         buttonFrame.anchoredPosition = currentPos;
+        exchangeFrame.anchoredPosition = currentPos;
     }
     IEnumerator MoveToShrink(float timeToCenter)
     {
@@ -103,9 +178,12 @@ public class UpdradeShipCanvas : MonoBehaviour
             float setSize = initialSize + (widthDiff * (currentTime / timeToCenter));
             shipFrame.sizeDelta = new Vector2(setSize, shipFrame.rect.height);
             buttonFrame.sizeDelta = new Vector2(setSize, shipFrame.rect.height);
+            exchangeFrame.sizeDelta = new Vector2(setSize, shipFrame.rect.height);
             yield return null;
         }
         shipFrame.sizeDelta = new Vector2(100, shipFrame.rect.height);
+        buttonFrame.sizeDelta = new Vector2(100, shipFrame.rect.height);
+        exchangeFrame.sizeDelta = new Vector2(100, shipFrame.rect.height);
         StartCoroutine(MoveToBottom(0.2f));
     }
 }
